9150889a52bd01b8c2115404a28075232230e23b
GabrielADKN <adoukonoubienheureux@gmail.com>
2023-11-27 18:51:32 -0500

    adding test

--- test.py
+++ test.py
@@ -6,5 +6,50 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
+    def test_homepage(self):
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Boggle Game', response.data)
+
+    def test_start_game(self):
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'board', session)
+
+    def test_guess_word(self):
+        with self.client:
+            with self.client.session_transaction() as sess:
+                sess['board'] = [['A', 'B', 'C', 'D', 'E'],
+                                 ['F', 'G', 'H', 'I', 'J'],
+                                 ['K', 'L', 'M', 'N', 'O'],
+                                 ['P', 'Q', 'R', 'S', 'T'],
+                                 ['U', 'V', 'W', 'X', 'Y']]
+            response = self.client.post('/guess', data={'word': 'DOG'})
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'result', session)
+            self.assertIn(b'word', session['result'])
+            self.assertIn(b'score', session['result'])
+
+    def test_invalid_word(self):
+        with self.client:
+            with self.client.session_transaction() as sess:
+                sess['board'] = [['A', 'B', 'C', 'D', 'E'],
+                                 ['F', 'G', 'H', 'I', 'J'],
+                                 ['K', 'L', 'M', 'N', 'O'],
+                                 ['P', 'Q', 'R', 'S', 'T'],
+                                 ['U', 'V', 'W', 'X', 'Y']]
+            response = self.client.post('/guess', data={'word': 'INVALID'})
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'result', session)
+            self.assertIn(b'word', session['result'])
+            self.assertIn(b'score', session['result'])
+            self.assertEqual(session['result']['score'], 0)
+
+if __name__ == '__main__':
+    unittest.main()
